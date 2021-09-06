fbf48ecb4a96952ab9c7a3a9969957e7365b73dc
shumaikunkun <s1711520@s.tsukuba.ac.jp>
2021-09-07 03:48:25 +0900

    regard default (non-click) condition as 1st phase

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const maps = [ "./prefecture.svg"]
 const containers = document.querySelectorAll( '.map' )
 
-const color_phase = ["rgb(200, 200, 200)", "rgb(150, 150, 150)", "rgb(100, 100, 100)", "rgb(50, 50, 50)", "rgb(0, 0, 0)"]
+const color_phase = ["rgb(200, 200, 200)", "rgb(150, 150, 150)", "rgb(100, 100, 100)", "rgb(50, 50, 50)"]
 
 maps.forEach( async ( map, index ) => {
   const res = await fetch( map )
@@ -9,27 +9,28 @@ maps.forEach( async ( map, index ) => {
   if ( res.ok ) {
     const svg = await res.text()
     containers[ index ].innerHTML = svg
+
     const prefs = document.querySelectorAll( '.geolonia-svg-map .prefecture' )
 
     prefs.forEach( ( pref ) => {
       pref.addEventListener( 'click', ( event ) => {
         const rgb = event.currentTarget.style.fill
+        console.log("変更前: "+rgb);
+        console.log(rgb==false);
+        console.log(rgb==color_phase[0]);
+
+        console.log(pref);
+
         if (rgb == false) {
           event.currentTarget.style.fill = color_phase[0]
         }else if (color_phase.includes(rgb)) {
-          index = color_phase.indexOf(rgb)
-          if (index >= color_phase.length-1) {
-            event.currentTarget.style.fill = color_phase[0]
+          i = color_phase.indexOf(rgb)
+          if (i >= color_phase.length-1) {
+            event.currentTarget.style.fill = null
           }else{
-            event.currentTarget.style.fill = color_phase[index+1]
+            event.currentTarget.style.fill = color_phase[i+1]
           }
         }
-
-        console.log(pref);
-        console.log(event.currentTarget.style.fill);
-        console.log(event.currentTarget.style.fill==color_phase[0]);
-        console.log(event.currentTarget.style==false);
-
       } )
     } )
   }
