96dc7456451e1439fa94bb0b4492cc9bdc8102b5
shumaikunkun <s1711520@gmail.com>
2021-09-07 00:02:42 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const maps = [ "./prefecture.svg"]
+const containers = document.querySelectorAll( '.map' )
+
+maps.forEach( async ( map, index ) => {
+  const res = await fetch( map )
+
+  if ( res.ok ) {
+    const svg = await res.text()
+    containers[ index ].innerHTML = svg
+    const prefs = document.querySelectorAll( '.geolonia-svg-map .prefecture' )
+
+    prefs.forEach( ( pref ) => {
+      pref.addEventListener( 'mouseover', ( event ) => {
+        event.currentTarget.style.fill = "#555555"
+      } )
+      pref.addEventListener( 'mouseleave', ( event ) => {
+        event.currentTarget.style.fill = ""
+      } )
+    } )
+  }
+} )
